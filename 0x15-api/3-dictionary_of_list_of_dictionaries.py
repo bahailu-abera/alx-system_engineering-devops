938ebe9f2e233d655859dfd44377660359bdccd2
bahailu <aberabahailu4@gmail.com>
2022-10-18 00:12:23 +0300

    make it one dict

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,11 +7,9 @@ import requests
 import sys
 
 
-def export_to_json(username, id, todos):
-    """ Exports the fetched data to json file format. """
-    userid = id
+def filter_item(username, todos):
+    """ Filters usefull items from the todos dictionary """
     file_name = "todo_all_employees.json"
-
     todo_list = []
 
     for todo in todos:
@@ -20,8 +18,13 @@ def export_to_json(username, id, todos):
         del todo['id']
         todo['username'] = username
         todo_list.append(todo)
-    todo_dict = {}
-    todo_dict[userid] = todo_list
+
+    return todo_list
+
+
+def export_to_json(todo_dict):
+    """ Exports the fetched data to json file format. """
+    file_name = "todo_all_employees.json"
     json_string = json.dumps(todo_dict)
 
     with open(file_name, 'a') as fs:
@@ -32,13 +35,17 @@ def fetch_todos(base_url='https://jsonplaceholder.typicode.com/users'):
     """ Fetch the todos of the user with id = @id """
 
     users = requests.get(base_url)
+    my_dict = {}
     for user in users.json():
         todo_url = base_url + "/{}/todos".format(user.get('id'))
         todos = requests.get(todo_url)
         username = user.get('username')
         id = user.get('id')
         todos = todos.json()
-        export_to_json(username, id, todos)
+        lst = filter_item(username, todos)
+        my_dict[id] = lst
+
+    export_to_json(my_dict)
 
 
 if __name__ == "__main__":
