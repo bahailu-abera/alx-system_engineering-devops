4efa61c4e6589265493ec0bf636225be7b386dd4
bahailu <aberabahailu4@gmail.com>
2022-11-06 23:29:05 +0300

    puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # pupper manifest to edit a file content
 exec { 'sed':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path     => ['usr/bin:/usr/sbin:/bin']
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => ['usr/bin:/usr/sbin:/bin']
 }
