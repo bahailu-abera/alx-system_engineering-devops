f2b356fb73551fafeab7342df5c7e31a1321da10
bahailu <aberabahailu4@gmail.com>
2023-01-11 14:16:05 +0300

    sort

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,34 +5,34 @@ Module pagination
 import requests
 
 
-def count_words(subreddit, word_list, after='', words={}):
+def count_words(subreddit, word_list, after=None, words={}):
     """
     A recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    if after is None:
-        print("finished")
-        print(words)
-        lst = sorted([(v, k) for k, v in words.items()], reverese=True)
-        for value, key in lst:
-            print("{}: {}".format(key, value))
-    if after:
-        url = url + "?after=" + after
-
     try:
+        payload = {'after': after}
         res = requests.get(url, allow_redirects=False,
+                           params=payload,
                            headers={'User-Agent': 'Pagination-Example'})
         res = res.json()
-        data = res.get('data').get('children')
+        posts = res.get('data').get('children')
         after = res.get('data').get('after')
-        for post in data:
+        for post in posts:
             title = post.get('data').get('title')
             for word in word_list:
-                word = word.lower()
-                if word in title.lower():
-                    words[word] = words.get(word, 0) + 1
+                try:
+                    words[word] += title.lower().split().count(word.lower())
+                except KeyError:
+                    words[word] = title.lower().split().count(word.lower())
+        if after is None:
+            lst = sorted(words.items(), key=lambda t: (-a[1], a[0]))
+            for key, value in lst:
+                if value:
+                    print("{:s}: {:s}".format(key, value))
+            return
         return count_words(subreddit, word_list, after, words)
     except Exception:
         return None
